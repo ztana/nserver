4f4aeaac5676f1d788a64a655d33374039aee8e2
ztana <peng.zuo.1990@gmail.com>
2020-02-17 10:18:53 +0800

    port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const multer = require('multer') // v1.0.5
 const upload = multer() // for parsing multipart/form-data
 var cache = require('lru-node-cache')
 var cors = require('cors')
-const PORT = 8088
+const PORT = 80
 
 let mcache = new cache.LRU(7000);
 
